347188bdf891ea063dad525dd634bf62a331ef3d
José Miguel Sánchez García <soy.jmi2k@gmail.com>
2021-09-24 22:46:36 +0200

    Add reelay-testzone (not working yet)

---
+++ components/reelay-testzone/src/CMakeFiles/reelay_testzone_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reelay_testzone_autogen"
+  "reelay_testzone_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/reelay_testzone_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

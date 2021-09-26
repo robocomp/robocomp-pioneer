763bb26b5bf9e82d4aac7b808732fd1dd5d74226
pbustos <pbustos@unex.es>
2021-09-26 13:05:11 +0200

    removing garbage

--- components/reelay-testzone/src/CMakeFiles/ICES__home_jmi2k_robocomp_components_reelay-testzone_src.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ICES__home_jmi2k_robocomp_components_reelay-testzone_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
